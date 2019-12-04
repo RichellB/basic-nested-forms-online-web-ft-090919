989a436f006ca14d945123b6797f07ef425565f0
Richell Bonfield <richell.bonfield12@gmail.com>
2019-12-04 16:28:45 +0000

    Automatically backed up by Learn

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -16,15 +16,7 @@ def new
  
   private
 
-    params.require(:person).permit(
-    :name,
-    addresses_attributes: [
-      :street_address_1,
-      :street_address_2,
-      :city,
-      :state,
-      :zipcode,
-      :address_type
-    ]
-  )
+  def person_params
+    params.require(:person).permit(:name)
   end
+end
